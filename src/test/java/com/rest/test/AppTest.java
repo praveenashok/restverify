011e671602e4901a1d4d7d3a0e2f2285c5ffd1cd
praveeng <praveen.ashok@glassbeam.com>
2020-03-16 10:36:39 +0530

    Comitting final set of changes

--- src/test/java/com/rest/test/AppTest.java
+++ src/test/java/com/rest/test/AppTest.java
@@ -13,10 +13,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
+import org.testng.asserts.SoftAssert;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
@@ -26,8 +26,10 @@ public class AppTest{
     ExtentHtmlReporter htmlReporter;
     public static ExtentReports extent;
     public static ExtentTest test;
+    public static String domain="reqres.in";
     private static final Logger LOG = LoggerFactory.getLogger(AppTest.class );
-	
+    private static SoftAssert softAssert = new SoftAssert();
+
     public static List<String> getfileasList(Path path){
 	List<String> list=new ArrayList<String>();
 	try {
@@ -38,7 +40,7 @@ public class AppTest{
 	return list;
 	}
 
-    @BeforeTest
+    @BeforeSuite
     public void startReport() {
     PropertyConfigurator.configure(System.getProperty("user.dir")+"/src/test/resources/log4j.xml");
     htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir")+"/test-output/extentReport.html");
@@ -46,68 +48,78 @@ public class AppTest{
     extent.attachReporter(htmlReporter);
     }
 
-    @Test(dataProvider = "list")
-    public static void restapiresp(List<String> url1,List<String> url2) {
+    @Test(dataProvider ="lists")
+    public void restapiresp(List<String> url1,List<String> url2) {
 	Response resp1 = null,resp2 = null;
-	for(int i=0;i<url1.size();i++) {
+	for(int i=0;i<file1.size();i++) {
+	try {
 	LOG.info("Testing for "+(i+1)+" TC "+url1.get(i)+" VS "+url2.get(i));
-    if(!url1.get(i).contains("reqres.in"))
+	if(!url1.get(i).contains(domain))
     {
     resp1=(Response) RestAssured.given().
     when().
-    get("https://reqres.in"+url1.get(i).toString()).
+    get("https://"+domain+url1.get(i)).
     then().
     statusCode(200).
     extract().response();
     }
-    if(!url2.get(i).contains("reqres.in"))
+    if(!url2.get(i).contains(domain))
     {
     resp2=(Response) RestAssured.given().
     when().
-    get("https://reqres.in"+url2.get(i).toString()).
+    get("https://"+domain+url2.get(i)).
     then().
     statusCode(200).
     extract().response();	
     }
-    if(url1.get(i).contains("reqres.in"))
+    if(url1.get(i).contains(domain))
 	{
     resp1=(Response) RestAssured.given().
     when().
-    get(url1.get(i).toString()).
+    get(url1.get(i)).
     then().
     statusCode(200).
     extract().response();
 	}
-    if(url2.get(i).contains("reqres.in"))
+    if(url2.get(i).contains(domain))
 	{
     resp2=(Response) RestAssured.given().
     when().
-    get(url2.get(i).toString()).
+    get(url2.get(i)).
     then().
     statusCode(200).
     extract().response();
 	}
-    try{
-	Assert.assertEquals(resp1.asString(),resp2.asString(),"\nTC Failed as both json not matching\n");
-	extent.createTest("TC "+(i+1)).pass(url1.get(i)+" equals "+url2.get(i));
-	LOG.info(url1.get(i) +" equals "+ url2.get(i));
-    } catch (AssertionError e) {
+    }catch(Exception e) {
+    	e.printStackTrace();
+    }
+	try{
+    	Assert.assertTrue(resp1.asString().equals(resp2.asString()));
+	    extent.createTest("TC "+(i+1)).pass(url1.get(i)+" equals "+url2.get(i));
+	    LOG.info(url1.get(i)+" equals "+ url2.get(i));
+    }catch (AssertionError e) {
     	e.printStackTrace();
     	extent.createTest("TC "+(i+1)).fail(url1.get(i)+" not equals "+url2.get(i));
-    	LOG.info(url1.get(i) +" not equals "+ url2.get(i));
-	}
-	}
+    	LOG.info(url1.get(i)+" not equals "+url2.get(i));
+    	softAssert.assertFalse(!resp1.asString().equals(resp2.asString()), "TC failed as both json not matching");
+    	}
+	continue;
+	}	
 }
 
     @AfterTest
     public static void endTest(){
     	extent.flush();
     }
-
-    @DataProvider(name = "list")
+    
+    @DataProvider(name = "lists")
     public static Object[][] hashmap() {
-	file1=AppTest.getfileasList(Paths.get(System.getProperty("user.dir")+"/src/test/java/resources/file1"));
-	file2=AppTest.getfileasList(Paths.get(System.getProperty("user.dir")+"/src/test/java/resources/file2"));
+    try {
+    file1=AppTest.getfileasList(Paths.get(System.getProperty("user.dir")+"/src/test/java/resources/file1"));
+    file2=AppTest.getfileasList(Paths.get(System.getProperty("user.dir")+"/src/test/java/resources/file2"));
+    }catch(Exception e) {
+    	e.printStackTrace();
+    }
     return new Object[][] {{file1,file2}};
 }
 }
